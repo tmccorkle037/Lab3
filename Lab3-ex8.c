4ae948ab537af2330c432c6bf7bcfc61a85d1ff4
tmccorkle037 <33167415+tmccorkle037@users.noreply.github.com>
2018-02-21 16:33:22 -0600

    fixes example 8

---
+++ Lab3-ex8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+int main(void)
+{
+	char *s, *dyn_s;
+	int ln;
+	printf("Enter the input string\n");
+	scanf("%s",s);
+	ln = strlen(s);
+	*dyn_s = (char*)malloc(strlen(s)+1);
+	dyn_s = s;
+	dyn_s[strlen(s)]='\0';
+	printf(dyn_s);
+	return 0;
+}
+
